5d3814212275985ff52a4a2d26f535adabdc099a
Akshat dwivedi <109903397+akshat148@users.noreply.github.com>
2024-03-06 15:43:44 +0530

    Update config2.php

--- config2.php
+++ config2.php
@@ -1,9 +1,5 @@
 <?php
-// SMTP Configuration
-define('SMTP_HOST', 'smtp.gmail.com');
-define('SMTP_PORT', 587);
-define('SMTP_USERNAME', 'official1481999@gmail.com');
-define('SMTP_PASSWORD', 'onwvydajdmkytqjj');
+
 
 // Server Cap Configuration
 define('SERVER_CAP', 2000);
