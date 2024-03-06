9716e88b9abbb7775e498369bc731c366d7ddeb1
Akshat dwivedi <109903397+akshat148@users.noreply.github.com>
2024-03-06 15:26:46 +0530

    Add files via upload

---
+++ config2.php
@@ -0,0 +1,13 @@
+<?php
+// SMTP Configuration
+define('SMTP_HOST', 'smtp.gmail.com');
+define('SMTP_PORT', 587);
+define('SMTP_USERNAME', 'official1481999@gmail.com');
+define('SMTP_PASSWORD', 'onwvydajdmkytqjj');
+
+// Server Cap Configuration
+define('SERVER_CAP', 2000);
+
+
+define('EXPIRATION_DATE', strtotime('2025-12-30'));
+?>
